d5a6e1e743e6f7c2c7adc9d5849c9dacebc03014
Aliber009 <57564469+Aliber009@users.noreply.github.com>
2022-04-01 12:13:25 +0000

    Update smpt.js

--- back-end/config/smpt.js
+++ back-end/config/smpt.js
@@ -8,8 +8,8 @@ module.exports = {
     port: 465, // Port
     secure: true, // this is true as port is 465
     auth: {
-      user: "developers@digieye.io", // Gmail username
-      pass: "Digieye@2022", // Gmail password
+      user:  // Gmail username
+      pass:  // Gmail password
     },
   },
   webURL: 'https://localhost:5100/',
